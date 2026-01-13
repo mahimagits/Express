812d90cbfe807dd2fd5e49ed6eb2355810b60a78
Mahima <mahimajha2607@gmail.com>
2026-01-13 21:08:32 +0530

    File Change

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const port = 8080;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
-    res.render("home.ejs");
+    res.render("home");
 })
 
 app.listen(port, () => {
