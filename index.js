2cabeb446741b07dfdfce9afe6d4eee034d5b86a
Mahima <mahimajha2607@gmail.com>
2026-01-13 19:50:31 +0530

    File Change

--- index.js
+++ index.js
@@ -8,10 +8,25 @@ app.listen(port, () => {
     console.log(`App is listening at port ${port}`);
 })
 
-app.use((req, res) => {
-    console.log("request received");
-    res.send({
-        fruit : "apple",
-        color : "red",
-    });
+
+app.get("/", (req, res) => {
+    res.send("You contacted root path");
 })
+
+app.get("/search", (req, res) => {
+    res.send("You contacted search path");
+})
+
+app.get("/help", (req, res) => {
+    res.send("You contacted help path");
+})
+
+app.get("*", (req, res) => {
+    res.send("This is not a path"); //Any other path than the above mentioned path
+})
+
+// app.use((req, res) => {
+//     console.log("request received");
+//     let code = "<h1>Fruits</h1><ul><li>apple</li><li>banana</li><li>orange</li></ul>";
+//     res.send(code);
+// })
