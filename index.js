1fb58cf8190cd01a612a5f1a8c547a31b7c0e6e5
Mahima <mahimajha2607@gmail.com>
2026-01-14 20:13:31 +0530

    File Change

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ const port = 8080;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-app.get("/", (req, res) => {
-    res.render("home");
-})
-
-app.get("/rollDice", (req, res) => {
-    let diceVal = Math.floor(Math.random() * 6) + 1;
-    res.render("rollDice.ejs", { num : diceVal});
+app.get("/ig/:username", (req, res) => {
+    let { username } = req.params;
+    console.log(username);
+    res.render("instagram.ejs", {username});
 })
 
 app.listen(port, () => {
